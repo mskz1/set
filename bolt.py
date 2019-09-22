8939b56b0fbcca029459482387af698b4129fca1
mskz1 <mskz.tsuji@me.com>
2019-09-22 22:17:43 +0900

    boltモジュール作業中

--- bolt.py
+++ bolt.py
@@ -1,19 +1,57 @@
 # -*- coding: utf-8 -*-
 
+# class Bolt:
+#     def __init__(self):
+#         self._name = ""
+#         self._hole_dia = 0
+#         self._Qa_long = 0.
+#         self._Qa_short = 0.
+#         self._Na_long = 0.
+#         self._Na_short = 0.
 
-class Bolt:
-    def __init__(self):
-        self._name = ""
-        self._hole_dia = 0
-        self._Qa_long = 0.
-        self._Qa_short = 0.
-        self._Na_long = 0.
-        self._Na_short = 0.
 
+# class K:
+#     M16 = 'M16'
+#     M20 = 'M20'
+#     M22 = 'M22'
+#     M24 = 'M24'
+#     M27 = 'M27'
+#     M30 = 'M30'
+#     F10T = 'F10T'
+#     F8T = 'F8T'
+#     Qal = 'Qal'  # 長期許容せん断力(kN) １面せん断
+#     Qas = 'Qas'  # 短期許容せん断力(kN) １面せん断
+#     Dia = 'Dia'  # ボルト軸径(mm)
+#     Hole_dia = 'Hole_dia'  # ボルト孔径(mm)
+
+
+M16 = 'M16'
+M20 = 'M20'
+M22 = 'M22'
+M24 = 'M24'
+M27 = 'M27'
+M30 = 'M30'
+
+F10T = 'F10T'
+F8T = 'F8T'
+
+Qal = 'Qal'  # 長期許容せん断力(kN) １面せん断
+Qas = 'Qas'  # 短期許容せん断力(kN) １面せん断
+
+Tal = 'Tal'  # 長期許容引張力(kN)
+Tas = 'Tas'  # 短期期許容引張力(kN)
+
+Dia = 'Dia'  # ボルト軸径(mm)
+Hole_dia = 'Hole_dia'  # ボルト孔径(mm)
 
 # 寸法はmm, 力はkN
-HTB_M16 = dict(name='M16', dia=16, hole_dia=18, Qal=30.2, Qas=45.3)
+# HTB_M16 = dict(name='M16', dia=16, hole_dia=18, Qal=30.2, Qas=45.3)
 HTB_M20 = dict(name='M20', dia=20, hole_dia=22, Qal=47.1, Qas=70.6)
+
+# HTB_M16 = dict(name='M16', dia=16, hole_dia=18, Qal=30.2, Qas=45.3)
+
+# HTB_M16 = {K.Dia: 16, K.Qal: 30.2, K.Qas: 45.3}
+HTB_M16 = {Dia: 16, Qal: 30.2, Qas: 45.3}
 
 HTB_SPC = {}
 HTB_SPC['M16'] = HTB_M16
@@ -23,5 +61,48 @@ HTB_SIZE = ['M16', 'M20', 'M22', 'M24', 'M27', 'M30']
 HTB_STRENGTH = ['F8T', 'F10T']
 
 
-def htb_spec(size, type, term):
-    return
+# def htb_spec(size, type, term=None, F_type=K.F10T):
+#     if F_type == K.F10T:
+#         if size == K.M16:
+#             if type == K.Qal:
+#                 return 30.2
+#             elif type == K.Qas:
+#                 return 45.3
+#         elif size == K.M20:
+#             if type == K.Qal:
+#                 return 30.2
+#             elif type == K.Qas:
+#                 return 45.3
+#         elif size == K.M22:
+#             pass
+#         elif size == K.M24:
+#             pass
+#         elif size == K.M27:
+#             pass
+#
+#     elif F_type == K.F8T:
+#         pass
+
+
+def htb_spec(size, type, term=None, F_type=F10T):
+    if F_type == F10T:
+        if size == M16:
+            if type == Qal:
+                return 30.2
+            elif type == Qas:
+                return 45.3
+        elif size == M20:
+            if type == Qal:
+                return 30.2
+            elif type == Qas:
+                return 45.3
+        elif size == M22:
+            pass
+        elif size == M24:
+            pass
+        elif size == M27:
+            pass
+
+    elif F_type == F8T:
+        pass
+
--- test/test_bolt.py
+++ test/test_bolt.py
@@ -4,7 +4,12 @@ import pytest
 from bolt import HTB_SPC
 from bolt import htb_spec
 
+# from bolt import K
+
+
 
 def test_bolt_object():
     assert HTB_SPC['M16']['Qal'] == 30.2
-    assert htb_spec(size='M16', type='Q', term='LONG') == 30.2
+    # assert HTB_SPC[K.M16][K.Qal] == 30.2
+    # assert htb_spec(size=K.M16, type=K.Qal) == 30.2
+    assert htb_spec(size='M16', type='Qal') == 30.2
