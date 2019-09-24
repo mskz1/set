767e27900f819c3bc88821648d42104ef2ced1f8
mskz1 <mskz.tsuji@me.com>
2019-09-24 19:42:49 +0900

    boltモジュール作業中

--- bolt.py
+++ bolt.py
@@ -13,10 +13,8 @@ TERMS = ['LONG', 'SHORT']
 LONG, SHORT = TERMS
 
 PROPERTY_NAMES = ['QA', 'TA', 'DIA', 'HOLE_DIA']
-
-
-QA = 'QA'  # 許容せん断力
-TA = 'TA'  # 許容引張力
+# 許容せん断力, 許容引張力, ボルト軸径(mm), ボルト孔径(mm)
+QA, TA, DIA, HOLE_DIA = PROPERTY_NAMES
 
 LONG_TERM = 'LONG_TERM'  # 長期
 SHORT_TERM = 'SHORT_TERM'  # 短期
@@ -28,23 +26,27 @@ Qas = 'Qas'  # 短期許容せん断力(kN) １面せん断
 Tal = 'Tal'  # 長期許容引張力(kN)
 Tas = 'Tas'  # 短期期許容引張力(kN)
 
-Dia = 'Dia'  # ボルト軸径(mm)
-Hole_dia = 'Hole_dia'  # ボルト孔径(mm)
-
 # 寸法はmm, 力はkN
 # HTB_M16 = dict(name='M16', dia=16, hole_dia=18, Qal=30.2, Qas=45.3)
 # HTB_M20 = dict(name='M20', dia=20, hole_dia=22, Qal=47.1, Qas=70.6)
 
-HTB_M16_F10T = {Dia: 16, Hole_dia: 18, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
-HTB_M20_F10T = {Dia: 20, Hole_dia: 22, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
-HTB_M22_F10T = {Dia: 22, Hole_dia: 24, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
-HTB_M24_F10T = {Dia: 24, Hole_dia: 26, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
-HTB_M27_F10T = {Dia: 27, Hole_dia: 29, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
-HTB_M30_F10T = {Dia: 30, Hole_dia: 32, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
-
+# HTB F10T データの設定
 HTB_SPC_F10T = {}
-HTB_SPC_F10T[M16] = HTB_M16_F10T
-HTB_SPC_F10T[M20] = HTB_M20_F10T
+HTB_SPC_F10T[M16] = {DIA: 16, HOLE_DIA: 18, Qal: 30.2, Qas: 45.2, Tal: 62.3, Tas: 93.5}
+HTB_SPC_F10T[M20] = {DIA: 20, HOLE_DIA: 22, Qal: 47.1, Qas: 70.7, Tal: 97.4, Tas: 146.}
+HTB_SPC_F10T[M22] = {DIA: 22, HOLE_DIA: 24, Qal: 57.0, Qas: 85.5, Tal: 118., Tas: 177.}
+HTB_SPC_F10T[M24] = {DIA: 24, HOLE_DIA: 26, Qal: 67.9, Qas: 102., Tal: 140., Tas: 210.}
+HTB_SPC_F10T[M27] = {DIA: 27, HOLE_DIA: 30, Qal: 85.9, Qas: 129., Tal: 177., Tas: 266.}
+HTB_SPC_F10T[M30] = {DIA: 30, HOLE_DIA: 33, Qal: 106., Qas: 159., Tal: 219., Tas: 329.}
+
+# HTB F8T データの設定
+HTB_SPC_F8T = {}
+HTB_SPC_F8T[M16] = {DIA: 16, HOLE_DIA: 18, Qal: 21.4, Qas: 32.1, Tal: 50.3, Tas: 75.4}
+HTB_SPC_F8T[M20] = {DIA: 20, HOLE_DIA: 22, Qal: 33.5, Qas: 50.2, Tal: 78.5, Tas: 117.}
+HTB_SPC_F8T[M22] = {DIA: 22, HOLE_DIA: 24, Qal: 40.5, Qas: 60.8, Tal: 95.0, Tas: 142.}
+HTB_SPC_F8T[M24] = {DIA: 24, HOLE_DIA: 26, Qal: 48.2, Qas: 72.3, Tal: 113., Tas: 169.}
+HTB_SPC_F8T[M27] = {DIA: 27, HOLE_DIA: 30, Qal: 61.0, Qas: 91.5, Tal: 143., Tas: 214.}
+HTB_SPC_F8T[M30] = {DIA: 30, HOLE_DIA: 33, Qal: 75.4, Qas: 113., Tal: 177., Tas: 265.}
 
 
 class ParameterError(Exception):
@@ -58,14 +60,16 @@ class ParameterError(Exception):
 def htb_spec(size=M16, prop=QA, term=LONG, strength=F10T):
     """指定されたHTBの性能値を返す。
 
-    サイズ(M16~M30)、性能名(QA,TA,DIA,HOLE_DIA)、長期・短期(LONG/SHORT)、HTB強度(F10T/F8T)を指定
+    サイズ(M16～M30)、性能名(QA/TA/DIA/HOLE_DIA)、長期・短期(LONG/SHORT)、HTB強度(F10T/F8T)を指定
     return : 許容せん断力(kN)、許容引張力(kN)、軸径(mm)、孔径(mm)"""
 
     if size in HTB_SIZES and prop in PROPERTY_NAMES and term in TERMS and strength in HTB_STRENGTHS:
+        SPC = None
         if strength == F10T:
             SPC = HTB_SPC_F10T
         elif strength == F8T:
             SPC = HTB_SPC_F8T
+
         actual_prop = ''
         if prop == QA:
             if term == LONG:
@@ -73,10 +77,16 @@ def htb_spec(size=M16, prop=QA, term=LONG, strength=F10T):
             elif term == SHORT:
                 actual_prop = 'Qas'
         elif prop == TA:
-            pass
+            if term == LONG:
+                actual_prop = 'Tal'
+            elif term == SHORT:
+                actual_prop = 'Tas'
+        elif prop == DIA:
+            actual_prop = DIA
+        elif prop == HOLE_DIA:
+            actual_prop = HOLE_DIA
 
         return SPC[size][actual_prop]
-
     else:
         raise ParameterError('invalid parameter given')
 
--- test/test_bolt.py
+++ test/test_bolt.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-# from bolt import HTB_SPC
 from bolt import htb_spec
+from bolt import ParameterError
+from bolt import HTB_SIZES, HTB_STRENGTHS
 
 
-# from bolt import K
-
-
-
-def test_bolt_object():
-    # assert HTB_SPC['M16']['Qal'] == 30.2
-    # assert HTB_SPC[K.M16][K.Qal] == 30.2
-    # assert htb_spec(size=K.M16, type=K.Qal) == 30.2
+def test_bolt_spec():
     assert htb_spec(size='M16', prop='QA', term='LONG') == 30.2
+    assert htb_spec(size='M16', prop='QA', term='SHORT') == 45.2
+    assert htb_spec(size='M24', prop='TA', term='LONG') == 140.
+    assert htb_spec(size='M24', prop='TA', term='SHORT') != 140.
+    assert htb_spec(size='M24', prop='TA', term='SHORT') == 210.
+    with pytest.raises(ParameterError):
+        htb_spec(size='M17', prop='TA', term='SHORT')
+    with pytest.raises(ParameterError):
+        htb_spec(size='M16', prop='TA', term='SHORT-TIME')
+    with pytest.raises(ParameterError):
+        htb_spec(size='M16', prop='Tu')
+
+    assert htb_spec(size='M16', prop='DIA') == 16
+    assert htb_spec(size='M27', prop='DIA') == 27
+    assert htb_spec(size='M16', prop='HOLE_DIA') == 18
+    assert htb_spec(size='M27', prop='HOLE_DIA') == 30
+
+    assert htb_spec(size='M16', prop='QA', term='LONG', strength='F8T') == 21.4
+
+
+def test_bolt_spec_short_long():
+    for strg in HTB_STRENGTHS:
+        for size in HTB_SIZES:
+            qal = htb_spec(size=size, prop='QA', term='LONG', strength=strg)
+            qas = htb_spec(size=size, prop='QA', term='SHORT', strength=strg)
+            assert qas / qal == pytest.approx(1.5, abs=0.002)
