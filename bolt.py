b6f0be673bcae5f1982816ad4bc8f9b5d00c6dec
mskz1 <mskz.tsuji@me.com>
2019-09-24 12:28:49 +0900

    boltモジュール作業中

--- bolt.py
+++ bolt.py
@@ -3,45 +3,21 @@
 """ボルト／HTBに関するデータを返す関数"""
 
 
-class Bolt:
-    def __init__(self):
-        self._name = ""
-        self._hole_dia = 0
-        self._Qa_long = 0.
-        self._Qa_short = 0.
-        self._Na_long = 0.
-        self._Na_short = 0.
+HTB_SIZES = ['M16', 'M20', 'M22', 'M24', 'M27', 'M30']
+M16, M20, M22, M24, M27, M30 = HTB_SIZES  # アンパック代入
 
+HTB_STRENGTHS = ['F8T', 'F10T']
+F8T, F10T = HTB_STRENGTHS
 
-class K:
-    M16 = 'M16'
-    M20 = 'M20'
-    M22 = 'M22'
-    M24 = 'M24'
-    M27 = 'M27'
-    M30 = 'M30'
-    F10T = 'F10T'
-    F8T = 'F8T'
-    Qal = 'Qal'  # 長期許容せん断力(kN) １面せん断
-    Qas = 'Qas'  # 短期許容せん断力(kN) １面せん断
-    Dia = 'Dia'  # ボルト軸径(mm)
-    Hole_dia = 'Hole_dia'  # ボルト孔径(mm)
+TERMS = ['LONG', 'SHORT']
+LONG, SHORT = TERMS
 
+PROPERTY_NAMES = ['QA', 'TA', 'DIA', 'HOLE_DIA']
 
-M16 = 'M16'
-M20 = 'M20'
-M22 = 'M22'
-M24 = 'M24'
-M27 = 'M27'
-M30 = 'M30'
-
-F10T = 'F10T'
-F8T = 'F8T'
 
 QA = 'QA'  # 許容せん断力
 TA = 'TA'  # 許容引張力
-LONG = 'LONG'
-SHORT = 'SHORT'
+
 LONG_TERM = 'LONG_TERM'  # 長期
 SHORT_TERM = 'SHORT_TERM'  # 短期
 
@@ -58,8 +34,6 @@ Hole_dia = 'Hole_dia'  # ボルト孔径(mm)
 # 寸法はmm, 力はkN
 # HTB_M16 = dict(name='M16', dia=16, hole_dia=18, Qal=30.2, Qas=45.3)
 # HTB_M20 = dict(name='M20', dia=20, hole_dia=22, Qal=47.1, Qas=70.6)
-# HTB_M16 = dict(name='M16', dia=16, hole_dia=18, Qal=30.2, Qas=45.3)
-# HTB_M16 = {K.Dia: 16, K.Qal: 30.2, K.Qas: 45.3}
 
 HTB_M16_F10T = {Dia: 16, Hole_dia: 18, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
 HTB_M20_F10T = {Dia: 20, Hole_dia: 22, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
@@ -71,11 +45,6 @@ HTB_M30_F10T = {Dia: 30, Hole_dia: 32, Qal: 30.2, Qas: 45.3, Tal: 0, Tas: 0}
 HTB_SPC_F10T = {}
 HTB_SPC_F10T[M16] = HTB_M16_F10T
 HTB_SPC_F10T[M20] = HTB_M20_F10T
-
-HTB_SIZES = ['M16', 'M20', 'M22', 'M24', 'M27', 'M30']
-HTB_STRENGTHS = ['F8T', 'F10T']
-TERMS = ['LONG', 'SHORT']
-PROPERTY_NAMES = ['QA', 'TA', 'DIA', 'HOLE_DIA']
 
 
 class ParameterError(Exception):
@@ -112,47 +81,3 @@ def htb_spec(size=M16, prop=QA, term=LONG, strength=F10T):
         raise ParameterError('invalid parameter given')
 
 
-def old1_htb_spec(size, type, term=None, F_type=K.F10T):
-    if F_type == K.F10T:
-        if size == K.M16:
-            if type == K.Qal:
-                return 30.2
-            elif type == K.Qas:
-                return 45.3
-        elif size == K.M20:
-            if type == K.Qal:
-                return 30.2
-            elif type == K.Qas:
-                return 45.3
-        elif size == K.M22:
-            pass
-        elif size == K.M24:
-            pass
-        elif size == K.M27:
-            pass
-
-    elif F_type == K.F8T:
-        pass
-
-
-def old2_htb_spec(size=M16, prop='Q', term=None, F_type=F10T):
-    if F_type == F10T:
-        if size == M16:
-            if type == Qal:
-                return 30.2
-            elif type == Qas:
-                return 45.3
-        elif size == M20:
-            if type == Qal:
-                return 30.2
-            elif type == Qas:
-                return 45.3
-        elif size == M22:
-            pass
-        elif size == M24:
-            pass
-        elif size == M27:
-            pass
-
-    elif F_type == F8T:
-        pass
