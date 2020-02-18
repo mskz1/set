f23e28f019b4518ec60fefe9871507d6e29ef6bf
mskz1 <mskz.tsuji@me.com>
2020-02-18 17:59:49 +0900

    make_section_db関数　作成

--- cross_section/test_xs_section.py
+++ cross_section/test_xs_section.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
+import pytest,pprint
 
 from .xs_section import *
 
@@ -18,3 +18,20 @@ def test_xs_H_sec_property():
     assert set(xs_section_property('HS20')) == {'H', 'An', 'Ix', 'Iy', 'ix', 'iy', 'Zx', 'Zy'}
     assert xs_section_property('HS20', 'An') == 26.67  # unit:cm2
     assert xs_section_property('H-200x100x5.5x8', 'An') == 26.67  # unit:cm2
+
+def test_csv_parse():
+    hs = make_section_db(HS_SEC_DATA)
+    assert hs['H-200x100x5.5x8'][0]['An'] == 26.67
+    assert hs['H-600x200x11x17'][1] == ['H(mm)', 'B(mm)', 't1(mm)', 't2(mm)', 'r(mm)', 'An(cm**2)', 'W(kgf/m)', 'Ix(cm**4)', 'Iy(cm**4)', 'ix(cm)', 'iy(cm)', 'Zx(cm**3)', 'Zy(cm**3)', 'ib(cm)', 'η(-)', 'Zpx(cm**3)', 'Zpy(cm**3)']
+    # pprint.pprint(hs)
+    ps = make_section_db(P_SEC_DATA)
+    # pprint.pprint(ps)
+    assert ps['P-76.3x3.2'][0]['An'] == 7.35
+    assert ps['P-76.3x3.2'][0]['Zx'] == 12.9
+    cs = make_section_db(C_SEC_DATA)
+    # pprint.pprint(cs)
+    assert cs['C-100x50x20x2.3'][0]['Ix'] == 80.7
+
+def test_xs_section_property():
+    assert 1
+
--- test/test_section.py
+++ test/test_section.py
@@ -2,6 +2,7 @@
 # from unittest import TestCase
 
 __author__ = 'mskz'
+import pytest
 
 from cross_section.section import Section
 
