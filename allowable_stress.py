1cb08005ac67f0cf3685cda9c6d78b82718839cf
mskz1 <mskz.tsuji@me.com>
2020-03-03 12:56:26 +0900

    fb2_aij 実装

--- allowable_stress.py
+++ allowable_stress.py
@@ -14,9 +14,9 @@ def steel_ft(F=235):
     return F / 1.5
 
 
-def steel_fc(F=235, lambda_=100):
+def steel_fc_aij(F=235, lambda_=100):
     """
-    長期許容圧縮応力度(N/mm2)を返す。
+    長期許容圧縮応力度(N/mm2)を返す。日本建築学会版
     :param F:F値(N/mm2)
     :param lambda_:細長比
     :return:
@@ -29,3 +29,55 @@ def steel_fc(F=235, lambda_=100):
     else:
         fc = 0.277 * F / ((lambda_ / c_s_ratio) ** 2)
     return fc
+
+
+def steel_fc_bsl(F=235, lambda_=100):
+    """
+    長期許容圧縮応力度(N/mm2)を返す。建築基準法(Building Standard Law)版
+    :param F:F値(N/mm2)
+    :param lambda_:細長比
+    :return:
+    """
+    # critical slenderness ratio 限界細長比
+    c_s_ratio = 1500. / (F / 1.5) ** 0.5
+    if lambda_ <= c_s_ratio:
+        mu = 3.0 / 2.0 + 2.0 / 3.0 * ((lambda_ / c_s_ratio) ** 2)
+        fc = F * (1.0 - 2. / 5. * (lambda_ / c_s_ratio) ** 2) / mu
+    else:
+        fc = (18. / 65.) * F / ((lambda_ / c_s_ratio) ** 2)
+    return fc
+
+
+def steel_fb_aij(self, lb, i, C, h, Af):
+    """
+    許容曲げ応力度を返す　式(5.7,5.8)
+    N/mm2
+    lb :圧縮フランジの支点間距離(mm)
+    i :断面二次半径(mm)
+    C : 補正係数
+    h :はりのせい(mm)
+    Af :圧縮フランジの断面積(mm2)
+    """
+
+    fb1 = (1.0 - 0.4 * (lb / i) ** 2 / C / self._lamuda ** 2) * self.ft
+
+    fb2 = self.fb2_aij(lb, h, Af)
+
+    fb = min(max(fb1, fb2), self.ft)
+
+    return fb
+
+
+def steel_fb2_aij(F=235, lb=0, h=100, Af=30):
+    """
+    許容曲げ応力度を返す　AIJ_ASD_SS 式(5.8)
+    みぞ形断面など
+    """
+    try:
+        fb2 = 89000.0 / (lb * h / Af)
+    except ZeroDivisionError:
+        fb2 = steel_ft(F)
+
+    fb = min(fb2, steel_ft(F))
+
+    return fb
--- test/test_allowable_stress.py
+++ test/test_allowable_stress.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from allowable_stress import steel_ft, steel_fc
+from allowable_stress import steel_ft, steel_fc_aij, steel_fc_bsl, steel_fb2_aij, steel_fb_aij
 
 
 def test_steel_ft():
@@ -14,19 +14,37 @@ def test_steel_ft():
 
 
 def test_steel_fc():
-    assert steel_fc(F=235, lambda_=0) == 235 / 1.5
-    assert steel_fc(F=235, lambda_=50) == pytest.approx(135.274, abs=0.01)
-    assert steel_fc(F=235, lambda_=100) == pytest.approx(86.273, abs=0.01)
-    assert steel_fc(F=235, lambda_=150) == pytest.approx(41.5145, abs=0.01)
-    assert steel_fc(F=235, lambda_=200) == pytest.approx(23.3519, abs=0.01)
-    assert steel_fc(F=235, lambda_=250) == pytest.approx(14.9452, abs=0.01)
-    assert steel_fc(F=325, lambda_=0) == 325 / 1.5
-    assert steel_fc(F=325, lambda_=50) == pytest.approx(176.8466, abs=0.01)
-    assert steel_fc(F=325, lambda_=100) == pytest.approx(93.2114, abs=0.01)
-    assert steel_fc(F=325, lambda_=150) == pytest.approx(41.5145, abs=0.01)
-    assert steel_fc(F=325, lambda_=200) == pytest.approx(23.3519, abs=0.01)
-    assert steel_fc(F=325, lambda_=250) == pytest.approx(14.9452, abs=0.01)
+    assert steel_fc_aij(F=235, lambda_=0) == 235 / 1.5
+    assert steel_fc_aij(F=235, lambda_=50) == pytest.approx(135.274, abs=0.01)
+    assert steel_fc_aij(F=235, lambda_=100) == pytest.approx(86.273, abs=0.01)
+    assert steel_fc_aij(F=235, lambda_=150) == pytest.approx(41.5145, abs=0.01)
+    assert steel_fc_aij(F=235, lambda_=200) == pytest.approx(23.3519, abs=0.01)
+    assert steel_fc_aij(F=235, lambda_=250) == pytest.approx(14.9452, abs=0.01)
+    assert steel_fc_aij(F=325, lambda_=0) == 325 / 1.5
+    assert steel_fc_aij(F=325, lambda_=50) == pytest.approx(176.8466, abs=0.01)
+    assert steel_fc_aij(F=325, lambda_=100) == pytest.approx(93.2114, abs=0.01)
+    assert steel_fc_aij(F=325, lambda_=150) == pytest.approx(41.5145, abs=0.01)
+    assert steel_fc_aij(F=325, lambda_=200) == pytest.approx(23.3519, abs=0.01)
+    assert steel_fc_aij(F=325, lambda_=250) == pytest.approx(14.9452, abs=0.01)
 
-@pytest.mark.skip()
+
+def test_steel_fc_compare_aij_to_bsl():
+    # 学会式と基準法式の比較
+    for l in range(0, 251):
+        # print(l)
+        assert steel_fc_aij(F=235, lambda_=l) == pytest.approx(steel_fc_bsl(F=235, lambda_=l), abs=0.055)
+
+
+# @pytest.mark.skip()
 def test_steel_fb():
-    assert steel_fb1(F=235) == 0.
+    assert steel_fb2_aij(F=235, lb=0) == 235 / 1.5
+    assert steel_fb2_aij(F=235, lb=1000, h=200, Af=800) == 235 / 1.5
+    assert steel_fb2_aij(F=235, lb=2000, h=200, Af=800) == pytest.approx(235 / 1.5)
+    assert steel_fb2_aij(F=235, lb=3000, h=200, Af=800) == pytest.approx(118.6666)
+    assert steel_fb2_aij(F=235, lb=4000, h=200, Af=800) == pytest.approx(89.0)
+    assert steel_fb2_aij(F=235, lb=5000, h=200, Af=800) == pytest.approx(71.2)
+    assert steel_fb2_aij(F=235, lb=6000, h=200, Af=800) == pytest.approx(59.3333)
+    assert steel_fb2_aij(F=235, lb=7000, h=200, Af=800) == pytest.approx(50.8571)
+    assert steel_fb2_aij(F=235, lb=8000, h=200, Af=100 * 8) == pytest.approx(44.5)
+    assert steel_fb2_aij(F=235, lb=1000, h=500, Af=3200) == 235 / 1.5
+    assert steel_fb2_aij(F=235, lb=5000, h=500, Af=200 * 16) == pytest.approx(113.92)
