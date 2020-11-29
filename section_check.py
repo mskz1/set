4e1acb4caf2ba65a5cba1f04bcbf8002efa9f50a
mskz1 <mskz.tsuji@me.com>
2020-11-29 17:07:58 +0900

    鉄骨断面算定モジュール 圧縮耐力WIP

--- section_check.py
+++ section_check.py
@@ -1,12 +1,41 @@
-from xs_section import xs_section_property
+from xs_section import xs_section_property, xs_section_name
+import allowable_stress as alws
 
 
-def tensile_allowable_force(sec, F, term):
+def allowable_tensile_force(sec, F, term):
+    """
+    部材の許容引張耐力を返す
+    :param sec: 断面名
+    :param F: F値 [N/mm2]
+    :param term: 荷重種別（短期・長期）"LONG" / "SHORT"
+    :return: [kN]
+    """
     area = xs_section_property(sec, 'An')
-    f = 1.0
+    term_factor = 1.0
     if term == 'LONG':
-        f = 1.0
+        term_factor = 1.0
     elif term == 'SHORT':
-        f = 1.5
-    print('Tas=', area * F / 10 * f, '[kN]')
-    pass
+        term_factor = 1.5
+    ft = alws.steel_ft(F)
+    return term_factor * area * ft / 10.
+
+
+def allowable_compressive_force(sec, F, term, lk):
+    area = xs_section_property(sec, 'An')
+    sec_full_name = xs_section_name(sec)
+    # TODO 断面により、iの名称が異なる。ix,iy,iu,iv,,,
+    if sec_full_name[0] in ['H']:
+        iy = xs_section_property(sec, 'iy')
+    if sec_full_name[0] in ['P']:
+        iy = xs_section_property(sec, 'ix')
+
+    term_factor = 1.0
+    if term == 'LONG':
+        term_factor = 1.0
+    elif term == 'SHORT':
+        term_factor = 1.5
+    # print(iy)
+
+    fc = alws.steel_fc_bsl(F, lk / iy)
+    # print(fc)
+    return term_factor * area * fc / 10.
--- test/test_steel_section_check.py
+++ test/test_steel_section_check.py
@@ -1,11 +1,40 @@
-from xs_section import *
-import allowable_stress as allw
-from section_check import tensile_allowable_force
+from section_check import allowable_tensile_force
+from section_check import allowable_compressive_force
+import pytest
+
+F_235 = 235  # N/mm2
+F_325 = 325
+LONG_TERM = "LONG"
+SHORT_TERM = "SHORT"
 
 
-def test_tensile_stress_check():
-    db = make_all_section_db()
-    sec = "H-200x100x5.5x8"
-    F = 235
-    term = "LONG"
-    print(tensile_allowable_force(sec, F, term))
+def test_allowable_tensile_force():
+    sec = "H-200x100x5.5x8"  # An = 26.67 cm2
+    assert allowable_tensile_force(sec, F_235, LONG_TERM) == pytest.approx(417.83)
+    assert allowable_tensile_force(sec, F_235, SHORT_TERM) == pytest.approx(626.745)
+    assert allowable_tensile_force(sec, F_325, LONG_TERM) == pytest.approx(577.85)
+    assert allowable_tensile_force(sec, F_325, SHORT_TERM) == pytest.approx(866.775)
+
+    sec = "L65*6"  # An = 7.527 cm2
+    assert allowable_tensile_force(sec, F_235, LONG_TERM) == pytest.approx(117.923)
+    assert allowable_tensile_force(sec, F_235, SHORT_TERM) == pytest.approx(176.8845)
+
+
+# @pytest.mark.skip("WIP")
+def test_allowable_compressive_force():
+    sec = "H-200x100x5.5x8"  # An = 26.67 cm2, iy=2.24[cm]
+    lk = 300.
+    assert allowable_compressive_force(sec, F_235, LONG_TERM, lk) == pytest.approx(138.9, abs=0.1)
+    assert allowable_compressive_force(sec, F_235, SHORT_TERM, lk) == pytest.approx(208.4, abs=0.1)
+
+    sec = "P89.1*2.8"
+    lk = 400.
+    assert allowable_compressive_force(sec, F_235, SHORT_TERM, lk) == pytest.approx(61.86, abs=0.1)
+
+
+
+@pytest.mark.skip("WIP")
+def test_allowable_bending_moment():
+    sec = "H-200x100x5.5x8"  # An = 26.67 [cm2], Zx=181 [cm3]
+
+    pass
