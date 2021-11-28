8a88ffbccd0093ac378830ca22b6748f82a4a6c5
mskz1 <mskz.tsuji@me.com>
2021-11-28 22:46:45 +0900

    strip_square_brackets() 一旦実装

--- tests/test_text_calc.py
+++ tests/test_text_calc.py
@@ -2,7 +2,7 @@
 import pytest
 import pprint
 
-from text_calc import strip_square_brackets,get_square_brackets_index
+from text_calc import strip_square_brackets, get_square_brackets_index
 
 """ VBA の textCalc udf のpython版
 
@@ -21,7 +21,8 @@ A2セルの計算結果は、
 
 """
 
-@pytest.mark.skip('tmp')
+
+# @pytest.mark.skip('まだ未実装')
 def test_strip_square_brackets():
     src_txt = '0.6[kN/m2]'
     res_txt = strip_square_brackets(src_txt)
@@ -31,9 +32,25 @@ def test_strip_square_brackets():
     res_txt = strip_square_brackets(src_txt)
     assert res_txt == '3.2'
 
+    src_txt = '0.6[DL:kN/m2]*3[幅:m]*4.5[高さ:m]'
+    res_txt = strip_square_brackets(src_txt)
+    assert res_txt == '0.6*3*4.5'
+
+    src_txt = '0123 [567] 9'
+    res_txt = strip_square_brackets(src_txt)
+    assert res_txt == '0123  9'
+
+    src_txt = '0123 [567] 9xxx [zz] x [zzz] xx [ zzzzz ] xx'
+    res_txt = strip_square_brackets(src_txt)
+    assert res_txt == '0123  9xxx  x  xx  xx'
+
+    src_txt = '0123［567] 9xxxx[zz] x [zzz] xx [ zzzzz ] xx'
+    res_txt = strip_square_brackets(src_txt)
+    assert res_txt == '0123 9xxxx x  xx  xx'
 
 
 def test_square_brackets_index():
+    # 補助関数のテスト
     # 有効な例
     src_txt = '0123 [567] 9'
     # print('=== source:{}'.format(src_txt))
@@ -55,20 +72,16 @@ def test_square_brackets_index():
     # get_square_brackets_index(src_txt)
     assert get_square_brackets_index(src_txt) == [(4, 8), (15, 18), (22, 26), (31, 39)]
 
-
     # エラーの例　バランスしていない。括弧の数が違う
     src_txt = 'x [ zz [ zz ] xxx [z] x [zz] xx [zz] xx'
-    print('=== source:{}'.format(src_txt))
+    # print('=== source:{}'.format(src_txt))
 
-    get_square_brackets_index(src_txt)
-    
-
-
+    with pytest.raises(ValueError, match='Square brackets are not balanced'):
+        idx = get_square_brackets_index(src_txt)
 
     # エラーの例　括弧が入れ子
     src_txt = 'xx [ zz [ z ] z ] xxx [zz] xx [zzz] xx [zz] xx'
-    print('=== source:{}'.format(src_txt))
+    # print('=== source:{}'.format(src_txt))
 
-    get_square_brackets_index(src_txt)
-
-
+    with pytest.raises(ValueError, match='Nested brackets'):
+        idx = get_square_brackets_index(src_txt)
--- text_calc.py
+++ text_calc.py
@@ -4,30 +4,40 @@
 
 
 def strip_square_brackets(src):
-    SQUARE_BRACKETS = ('[', ']')
+    """
+    src文字列から、角括弧で囲まれた部分を削除した文字列を返す
+    :param src:
+    :return:
+    """
+    idx_list = get_square_brackets_index(src)
+
     res = ''
-    for c in src:
-        if c not in SQUARE_BRACKETS:
-            res += c
+    s_idx = 0
+    for idx in idx_list:
+        e_idx = idx[0]
+        res += src[s_idx:e_idx]
+        s_idx = idx[1] + 1
+    res += src[s_idx:]
+
     return res
 
 
 def get_square_brackets_index(src):
-
+    """
+    補助関数　角括弧の位置をタプルのリストで返す
+    :param src:
+    :return:
+    """
     def zenkaku_to_hankaku():
         return src.replace('［', '[').replace('］', ']')
-
 
     def check_brackets_counts(src):
         num_of_open_brackets = src.count('[')
         num_of_close_brackets = src.count(']')
         if num_of_open_brackets != num_of_close_brackets:
-            print('** INVALID DATA ** --Square brackets are not balanced')
-            # raise ValueError('Square brackets are not balanced')
-            return
+            raise ValueError('Square brackets are not balanced')
 
     src2 = zenkaku_to_hankaku()
-    # print('==== modified src:{}'.format(src2))
     check_brackets_counts(src2)
 
     index_of_open_brackets = []
@@ -37,16 +47,12 @@ def get_square_brackets_index(src):
             index_of_open_brackets.append(i)
         elif c == ']':
             index_of_close_brackets.append(i)
-    # print(index_of_open_brackets)
-    # print(index_of_close_brackets)
     res = list(zip(index_of_open_brackets, index_of_close_brackets))
 
     prev_close_pos = 0
     for pos in res:
         if pos[0] > pos[1] or prev_close_pos > pos[0]:
-            print('** INVALID DATA ** --Nested brackets')
-            # raise ValueError('Nested brackets')
+            raise ValueError('Nested brackets')
         prev_close_pos = pos[1]
 
-    # print(res)
     return res
