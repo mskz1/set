7dc63e3b3598364da4de7c045e9256a1cb5d3ed9
mskz1 <mskz.tsuji@me.com>
2023-02-05 17:23:27 +0900

    var2val 作業中 任意の空白に対応

--- src/var2val.py
+++ src/var2val.py
@@ -1,4 +1,10 @@
-import re
+# import re
+
+SPACE_CHAR = '#'
+
+
+def replace_space_moji(txt):
+    return txt.replace(' ', ' ' + SPACE_CHAR + ' ')
 
 
 def add_space_around_operator(txt):
@@ -9,15 +15,14 @@ def add_space_around_operator(txt):
 
 
 def var2val(txt, var_val):
-    # result = re.split('[\+\-\*/\^]', txt)
-    txt = add_space_around_operator(txt)
-    txt_lst = txt.split(' ')
+    txt_m = add_space_around_operator(replace_space_moji(txt))
+    txt_lst = txt_m.split(' ')
     result = []
     for wd in txt_lst:
         if wd in var_val.keys():
             result.append(str(var_val[wd]))
+        elif wd == SPACE_CHAR:
+            result.append(' ')
         else:
             result.append(wd)
     return "".join(result)
-
-
--- tests/test_var2val.py
+++ tests/test_var2val.py
@@ -14,13 +14,26 @@ x,=,,9.8
 y,=,,11.5"""
 
 
+def test_replace_space_moji():
+    assert replace_space_moji('a + b') == 'a # + # b'
+    assert replace_space_moji('(a + b)') == '(a # + # b)'
+
+
 def test_add_space_around_operator():
     assert add_space_around_operator('a+b') == 'a + b'
+    assert add_space_around_operator('a + b') == 'a  +  b'
     assert add_space_around_operator('a+b-c/d+(2*x/y)^2') == 'a + b - c / d +  ( 2 * x / y )  ^ 2'
+    assert add_space_around_operator('(a+b)') == ' ( a + b ) '
+    assert add_space_around_operator('(a)+(b)') == ' ( a )  +  ( b ) '
 
 
 def test_var2val():
     var_dict = make_dict()
+    assert var2val("(a*b)+((c))", var_dict) == '(2*3)+((4))'
+    assert var2val("(a*b) + ((c))", var_dict) == '(2*3) + ((4))'
+
     assert var2val("a*b", var_dict) == '2*3'
     assert var2val("a+b+c-a-b-c*a/b^c", var_dict) == '2+3+4-2-3-4*2/3^4'
-    assert var2val("a + b + c - a - b - c*a/b^c", var_dict) == '2+3+4-2-3-4*2/3^4'
+    assert var2val("a + b + c - a - b - c*a/b^c", var_dict) == '2 + 3 + 4 - 2 - 3 - 4*2/3^4'
+    assert var2val("cos(radians(a))*b + tan(a/b) - sqrt(x)-((a/b)*c)", var_dict) \
+           == 'cos(radians(2))*3 + tan(2/3) - sqrt(1.1)-((2/3)*4)'
